493ced0b699d628b0322dc45c862593536b1c1ae
chulpan <chulpan423@gmail.com>
2022-01-16 21:37:56 +0300

    Chulpan: class BFood class GFood

--- Game.py
+++ Game.py
@@ -59,9 +59,64 @@ class Chel(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
 
+
+class GFood(pygame.sprite.Sprite):
+    food_image = load_image(total_food)
+    good_food = 0
+
+    def __init__(self, pos):
+        self.good_food = 0
+        super().__init__(all_sprites)
+        self.image = GFood.food_image
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+    def update(self):
+        # если ещё в небе
+        if not pygame.sprite.collide_mask(self, chel):
+            self.rect = self.rect.move(0, STEP)
+        else:
+            self.rect.x = 1500
+            self.rect.y = 900
+            self.image = load_image("sOjhw.png")
+            GFood.good_food += 1
+
+
+class BFood(pygame.sprite.Sprite):
+    food_image = load_image("poop.png")
+    bad_food = 0
+
+    def __init__(self, pos):
+        super().__init__(all_sprites)
+        self.image = BFood.food_image
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+    def update(self):
+        # если ещё в небе
+        if not pygame.sprite.collide_mask(self, chel):
+            self.rect = self.rect.move(0, STEP_BAD)
+        else:
+            # если падает на игрока
+            self.rect.x = 1500
+            self.rect.y = 900
+            self.image = load_image("sOjhw.png")
+            BFood.bad_food += 1
+
+
 pygame.init()
 running = True
 x, y = 0, 0
+
+if c != 0:
+    Chel.chel_image = load_image("good_cat.png")
+    GFood.food_image = load_image("fish.png")
+end_game = 0
+chel = Chel()
 
 while running:
     pygame.mouse.set_visible(True)
